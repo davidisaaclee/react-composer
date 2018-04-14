2e500298b0da2a9c20ea9b107c445ced6f482b4b
David Lee <david.isaac.lee@gmail.com>
2018-04-14 13:46:37 -0400

    Add function to convert native Selection to DocSelection model

--- src/index.js
+++ src/index.js
@@ -3,12 +3,25 @@ import * as R from 'ramda';
 import * as Doc from './Doc';
 import * as ParagraphUtils from './Paragraph';
 
+const errorMessages = {
+	couldNotFindParagraphIDForSelectionAnchor: anchorNode => `Could not find paragraph ID for selection anchor node: ${anchorNode}.`,
+	couldNotFindParagraphIDForSelectionFocus: focusNode => `Could not find paragraph ID for selection focus node: ${focusNode}.`
+};
+
 const k = {
 	paragraphIDAttributeKey: 'data-paragraph-id'
 };
 
 const Anchor = {
+	// make :: (ParagraphID, number) -> Anchor
 	make: (paragraphID, offset) => ({ paragraphID, offset })
+};
+
+const DocSelection = {
+	// make :: (Anchor, Anchor) -> DocSelection
+	// anchor - where the selection action began
+	// focus - where the selection action ended
+	make: (anchor, focus) => ({ anchor, focus }),
 };
 
 export const Edit = {
@@ -42,6 +55,8 @@ const EditorContainer = ({ editable = false, ...restProps }) => (
 function ancestorParagraphIDForNode(node) {
 	if (node == null) {
 		return null;
+	} else if (!(node instanceof Element)) {
+		return ancestorParagraphIDForNode(node.parentNode);
 	} else {
 		const paragraphID = node.getAttribute(k.paragraphIDAttributeKey);
 		if (paragraphID == null) {
@@ -51,6 +66,32 @@ function ancestorParagraphIDForNode(node) {
 		}
 	}
 }
+
+// docSelectionFromNativeSelection :: Selection -> DocSelection
+function docSelectionFromNativeSelection(selection) {
+	if (selection.anchorNode.nodeType !== Node.TEXT_NODE || selection.focusNode.nodeType !== Node.TEXT_NODE) {
+		// TODO
+		throw new Error("Attempted selection on non-text nodes.");
+	}
+
+	const anchorParagraphID = ancestorParagraphIDForNode(selection.anchorNode);
+	const focusParagraphID = ancestorParagraphIDForNode(selection.focusNode);
+
+	if (anchorParagraphID == null) {
+		throw new Error(errorMessages.couldNotFindParagraphIDForSelectionAnchor(selection.anchorNode));
+	}
+
+	// TODO: I think this shouldn't throw? What happens when no selection range?
+	if (focusParagraphID == null) {
+		throw new Error(errorMessages.couldNotFindParagraphIDForSelectionFocus(selection.focusNode));
+	}
+
+	// TODO: There should be a defined mapping between `Selection`'s offsets and model offsets.
+	const anchor = Anchor.make(anchorParagraphID, selection.anchorOffset);
+	const focus = Anchor.make(focusParagraphID, selection.focusOffset);
+	return DocSelection.make(anchor, focus);
+}
+
 
 // document :: Doc
 // onEdit :: RichText.Edit -> ()
