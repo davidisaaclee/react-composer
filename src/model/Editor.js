efa99a42b29935efa5a701c0dc2bfd7841b4a2f2
David Lee <david.isaac.lee@gmail.com>
2018-04-17 22:56:56 -0400

    Simplify Editor code

--- src/model/Editor.js
+++ src/model/Editor.js
@@ -3,10 +3,6 @@ import * as Edit from 'model/Edit';
 import * as DocSelection from 'model/DocSelection';
 import Doc from 'model/Doc';
 
-const lenses = {
-	selection: R.lensProp('selection')
-};
-
 // make :: DocSelection Pointer -> Editor
 // where Pointer ::= { key: string, offset: number }
 const make = (selection) => ({
@@ -27,11 +23,13 @@ function applyEdit(edit, prevDoc, nextDoc, editor) {
 			...cursorPointerInNewDoc,
 			offset: cursorPointerInNewDoc.offset + edit.text.length
 		};
+		const selection = 
+			DocSelection.makeCollapsed(cursorPointerAfterTextInsertion);
 
-		return R.set(
-			lenses.selection,
-			DocSelection.makeCollapsed(cursorPointerAfterTextInsertion),
-			editor);
+		return {
+			...editor,
+			selection
+		};
 	} else if (edit.type === Edit.types.replaceTextWithParagraphBreak) {
 		const pointerRange =
 			Doc.pointerRangeFromSelection(edit.selection, prevDoc);
@@ -43,11 +41,13 @@ function applyEdit(edit, prevDoc, nextDoc, editor) {
 
 		const cursorPosition =
 			Doc.makePointer(newParagraphID, 0);
+		const selection = 
+			DocSelection.makeCollapsed(cursorPosition);
 
-		return R.set(
-			lenses.selection,
-			DocSelection.makeCollapsed(cursorPosition),
-			editor);
+		return {
+			...editor,
+			selection
+		};
 	} else {
 		console.error("Unhandled edit type:", edit.type);
 		return editor;
