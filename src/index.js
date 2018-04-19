f422ff8aa4975d6b70d59dec91f8adf7cbc82ebe
David Lee <david.isaac.lee@gmail.com>
2018-04-19 13:01:13 -0400

    Expose more model types at the package level

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import Composer from './components/Composer';
 import Doc from './model/Doc';
 import * as Editor from './model/Editor';
+import * as Edit from './model/Edit';
+import * as DocSelection from './model/DocSelection';
+import * as Content from './model/Content';
 
-export { Doc, Editor };
+export { Doc, Editor, Edit, DocSelection, Content };
 export default Composer;
 
